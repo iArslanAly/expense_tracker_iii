d293fedd603fddbae490fe50d842a8af36953f75
Arslan Ali <iarslanaly@hotmail.com>
2024-04-16 23:47:05 +0500

    done with themeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_iii/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 177, 45, 201));
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme(
+          color: kColorScheme.secondaryContainer,
+          elevation: BorderSide.strokeAlignOutside,
+          margin: const EdgeInsets.all(10),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: kColorScheme.onPrimaryContainer,
+            backgroundColor: kColorScheme.primaryContainer,
+            elevation: 5,
+            shadowColor: kColorScheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                color: kColorScheme.primaryContainer,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              bodyLarge: TextStyle(
+                color: kColorScheme.onPrimaryContainer,
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+              bodyMedium: TextStyle(
+                color: kColorScheme.onPrimaryContainer,
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,20 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20),
+          margin: const EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 15,
+          ),
+          child: const Icon(
+            Icons.delete,
+            color: Colors.white,
+            size: 40,
+          ),
+        ),
         onDismissed: (direction) {
           onDeleteExpense(expenses[index]);
         },
